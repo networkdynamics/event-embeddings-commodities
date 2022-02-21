4583fc2274b1e814367fb3e45ad4e2936cd01c28
Ben Steel <bendavidsteel@gmail.com>
2022-02-21 17:45:00 -0500

    Updating scripts for new API

--- scripts/compile_dataset.py
+++ scripts/compile_dataset.py
@@ -1,27 +1,59 @@
-import argparse
+import logging
 import os
 
-from seldonite import collect, source
+from seldonite import collect, sources, run 
 from seldonite.helpers import utils
 
+def log(log_msg):
+    print(log_msg)
+    logging.info(log_msg)
+
 def main():
-    master_url = 'k8s://kubernetes.default.svc'
+    master_url = 'k8s://https://10.140.16.25:6443'
     db_connection_string = os.environ['MONGO_CONNECTION_STRING']
+
+    spark_conf = {
+        'spark.kubernetes.authenticate.driver.serviceAccountName': 'ben-dev',
+        'spark.kubernetes.driver.pod.name': 'seldonite-driver',
+        'spark.driver.host': 'seldonite-driver',
+        'spark.driver.port': '7078'
+    }
+
     db_name = 'political_events'
     db_table = 'news'
-    crawls = utils.get_all_cc_crawls()
+
+    this_file_dir = os.path.dirname(os.path.abspath(__file__))
+    log_path = os.path.join(this_file_dir, 'compile_dataset.log')
+    logging.basicConfig(filename=log_path, level=logging.INFO)
+
+    #crawls = utils.get_all_cc_crawls()
+    crawls = ['CC-MAIN-2018-34', 'CC-MAIN-2018-30', 'CC-MAIN-2018-26', 'CC-MAIN-2018-22', 
+              'CC-MAIN-2018-17', 'CC-MAIN-2018-13', 'CC-MAIN-2018-09', 'CC-MAIN-2018-05', 
+              'CC-MAIN-2017-51', 'CC-MAIN-2017-47', 'CC-MAIN-2017-43', 'CC-MAIN-2017-39', 
+              'CC-MAIN-2017-34', 'CC-MAIN-2017-30', 'CC-MAIN-2017-26', 'CC-MAIN-2017-22', 
+              'CC-MAIN-2017-17', 'CC-MAIN-2017-13', 'CC-MAIN-2017-09', 'CC-MAIN-2017-04', 
+              'CC-MAIN-2016-50', 'CC-MAIN-2016-44', 'CC-MAIN-2016-40', 'CC-MAIN-2016-36', 
+              'CC-MAIN-2016-30', 'CC-MAIN-2016-26', 'CC-MAIN-2016-22', 'CC-MAIN-2016-18', 
+              'CC-MAIN-2016-07', 'CC-MAIN-2015-48', 'CC-MAIN-2015-40', 'CC-MAIN-2015-35', 
+              'CC-MAIN-2015-32', 'CC-MAIN-2015-27', 'CC-MAIN-2015-22', 'CC-MAIN-2015-18', 
+              'CC-MAIN-2015-14', 'CC-MAIN-2015-11', 'CC-MAIN-2015-06', 'CC-MAIN-2014-52', 
+              'CC-MAIN-2014-49', 'CC-MAIN-2014-42', 'CC-MAIN-2014-41', 'CC-MAIN-2014-35', 
+              'CC-MAIN-2014-23', 'CC-MAIN-2014-15', 'CC-MAIN-2014-10', 'CC-MAIN-2013-48', 
+              'CC-MAIN-2013-20', 'CC-MAIN-2012', 'CC-MAIN-2009-2010', 'CC-MAIN-2008-2009']
     for crawl in crawls:
-        print(f"Starting pull for crawl: {crawl}")
-        cc_source = source.CommonCrawl()
+        log(f"Starting pull for crawl: {crawl}")
+
+        cc_source = sources.CommonCrawl()
         cc_source.set_crawls(crawl)
-        collector = collect.Collector(cc_source, master_url=master_url)
+        collector = collect.Collector(cc_source, master_url=master_url, num_executors=2, executor_cores=16, executor_memory='420g', spark_conf=spark_conf)
         collector.only_political_articles() \
                  .on_sites(['reuters.com']) \
-                 .exclude_in_path(['/sports/']) \
+                 .exclude_in_url(['*/sports/*', '*rus.reuters*']) \
                  .in_language(lang='eng')
 
-        collector.send_to_database(db_connection_string, db_name, db_table)
-        print(f"Finished pull for crawl: {crawl}")
+        runner = run.Runner(collector)
+        runner.send_to_database(db_connection_string, db_name, db_table)
+        log(f"Finished pull for crawl: {crawl}")
 
 if __name__ == '__main__':
     main()
--- scripts/test_send_database.py
+++ scripts/test_send_database.py
@@ -1,29 +1,87 @@
-import argparse
+import datetime
+import logging
 import os
 
-from seldonite import collect, source
+from seldonite import collect, sources
 from seldonite.helpers import utils
 
 def main():
     #master_url = 'k8s://kubernetes.default.svc'
     master_url = 'k8s://https://10.140.16.25:6443'
-    #db_connection_string = os.environ['MONGO_CONNECTION_STRING']
-    db_connection_string = 'mongodb://ndl-user:ndllab123@10.244.0.152:27017,10.244.0.154:27017,10.244.0.153:27017/?ssl=false'
+    db_connection_string = os.environ['MONGO_CONNECTION_STRING']
+    #db_connection_string = 'mongodb://ndl-user:ndllab123@10.244.0.152:27017,10.244.0.154:27017,10.244.0.153:27017/?ssl=false'
+
+    spark_conf = {
+        'spark.kubernetes.authenticate.driver.serviceAccountName': 'ben-dev',
+        'spark.kubernetes.driver.pod.name': 'seldonite-driver',
+        'spark.driver.host': 'seldonite-driver',
+        'spark.driver.port': '7078'
+    }
     
     db_name = 'political_events'
-    db_table = 'news'
-    crawls = utils.get_all_cc_crawls()
-    cc_source = source.CommonCrawl()
-    cc_source.set_crawls(crawls[0])
-    collector = collect.Collector(cc_source, master_url=master_url)
-    collector.limit_num_articles(100) \
-             .on_sites(['reuters.com']) \
-             .exclude_in_path(['/sports/']) \
-             .in_language(lang='eng') \
-             .url_only()
+    in_table = 'debug_news'
+    out_table = 'news'
+    cc_source = sources.MongoDB(db_connection_string, db_name, in_table)
+    #crawl = 'CC-MAIN-2020-34'
+    #cc_source.set_crawls(crawl)
+    
 
-    #collector.fetch().to_csv('./political_news.csv')
-    collector.send_to_database(db_connection_string, db_name, db_table)
+    start_dates = [
+        datetime.date(2010, 1, 1),
+        datetime.date(2015, 1, 1),
+        datetime.date(2018, 1, 1),
+        datetime.date(2019, 1, 1),
+        datetime.date(2020, 1, 1)
+    ]
+    end_dates = [
+        datetime.date(2015, 1, 1),
+        datetime.date(2018, 1, 1),
+        datetime.date(2019, 1, 1),
+        datetime.date(2020, 1, 1),
+        datetime.date(2021, 1, 1)
+    ]
+
+    min_start_date = datetime.date(2015, 1, 1)
+    max_end_date = datetime.date(2018, 1, 1)
+
+    start_date = min_start_date
+    end_date = max_end_date
+
+    this_file_dir = os.path.dirname(os.path.abspath(__file__))
+    log_path = os.path.join(this_file_dir, 'test_send_database.log')
+    logging.basicConfig(filename=log_path, level=logging.INFO)
+
+    # while end_date - start_date >= datetime.timedelta(days=1):
+    #     try:
+    #         collector = collect.Collector(cc_source, master_url=master_url, spark_conf=spark_conf)
+    #         collector.on_sites(['reuters.com']) \
+    #                 .exclude_in_url(['*/sports/*', '*rus.reuters*']) \
+    #                 .in_language(lang='eng') \
+    #                 .only_political_articles() \
+    #                 .in_date_range(start_date, end_date)
+
+    #         collector.fetch().to_csv('./debug_political_news.csv')
+    #         logging.info(str(start_date) + ' ' + str(end_date) + ' is good')
+    #         start_date = end_date
+    #         end_date = start_date + interval
+    #     except:
+    #         logging.info(str(start_date) + ' ' + str(end_date) + ' is bad')
+    #         interval = ((end_date - start_date) / 2)
+    #         end_date = start_date + interval
+    #collector.send_to_database(db_connection_string, db_name, out_table)
+
+    start_date = datetime.date(2018, 1, 1)
+    end_date = datetime.date(2020, 1, 1)
+
+    collector = collect.Collector(cc_source, master_url=master_url, spark_conf=spark_conf)
+    collector.on_sites(['reuters.com']) \
+            .exclude_in_url(['*/sports/*', '*rus.reuters*']) \
+            .in_language(lang='eng') \
+            .only_political_articles()
+            #.in_date_range(start_date, end_date)
+
+    collector.send_to_database(db_connection_string, db_name, out_table)
+    #collector.fetch().to_csv('./debug_political_news.csv')
 
 if __name__ == '__main__':
     main()
