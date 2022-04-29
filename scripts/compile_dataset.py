fad811308aba596658b67a1f29723865efe50a54
Ben Steel <bendavidsteel@gmail.com>
2022-04-29 09:55:23 -0400

    Changes to scripts

--- scripts/compile_dataset.py
+++ scripts/compile_dataset.py
@@ -10,6 +10,8 @@ def log(log_msg):
 def main():
     master_url = 'k8s://https://10.140.16.25:6443'
     db_connection_string = os.environ['MONGO_CONNECTION_STRING']
+    aws_access_key = os.environ['AWS_ACCESS_KEY']
+    aws_secret_key = os.environ['AWS_SECRET_KEY']
 
     spark_conf = {
         'spark.kubernetes.authenticate.driver.serviceAccountName': 'ben-dev',
@@ -26,19 +28,12 @@ def main():
     logging.basicConfig(filename=log_path, level=logging.INFO)
 
     #crawls = utils.get_all_cc_crawls()
-    crawls = ['CC-MAIN-2018-34', 'CC-MAIN-2018-30', 'CC-MAIN-2018-26', 'CC-MAIN-2018-22', 
-              'CC-MAIN-2018-17', 'CC-MAIN-2018-13', 'CC-MAIN-2018-09', 'CC-MAIN-2018-05', 
+    crawls = ['CC-MAIN-2018-13', 'CC-MAIN-2018-09', 'CC-MAIN-2018-05', 
               'CC-MAIN-2017-51', 'CC-MAIN-2017-47', 'CC-MAIN-2017-43', 'CC-MAIN-2017-39', 
               'CC-MAIN-2017-34', 'CC-MAIN-2017-30', 'CC-MAIN-2017-26', 'CC-MAIN-2017-22', 
               'CC-MAIN-2017-17', 'CC-MAIN-2017-13', 'CC-MAIN-2017-09', 'CC-MAIN-2017-04', 
               'CC-MAIN-2016-50', 'CC-MAIN-2016-44', 'CC-MAIN-2016-40', 'CC-MAIN-2016-36', 
-              'CC-MAIN-2016-30', 'CC-MAIN-2016-26', 'CC-MAIN-2016-22', 'CC-MAIN-2016-18', 
-              'CC-MAIN-2016-07', 'CC-MAIN-2015-48', 'CC-MAIN-2015-40', 'CC-MAIN-2015-35', 
-              'CC-MAIN-2015-32', 'CC-MAIN-2015-27', 'CC-MAIN-2015-22', 'CC-MAIN-2015-18', 
-              'CC-MAIN-2015-14', 'CC-MAIN-2015-11', 'CC-MAIN-2015-06', 'CC-MAIN-2014-52', 
-              'CC-MAIN-2014-49', 'CC-MAIN-2014-42', 'CC-MAIN-2014-41', 'CC-MAIN-2014-35', 
-              'CC-MAIN-2014-23', 'CC-MAIN-2014-15', 'CC-MAIN-2014-10', 'CC-MAIN-2013-48', 
-              'CC-MAIN-2013-20', 'CC-MAIN-2012', 'CC-MAIN-2009-2010', 'CC-MAIN-2008-2009']
+              'CC-MAIN-2016-30', 'CC-MAIN-2016-26', 'CC-MAIN-2016-22', 'CC-MAIN-2016-18']
 
     blacklist = ['*/sports/*', '*rus.reuters*', '*fr.reuters*', '*br.reuters*', '*de.reuters*', '*es.reuters*', \
                  '*lta.reuters*', '*ara.reuters*', '*it.reuters*', '*ar.reuters*', '*blogs.reuters*', '*graphics.reuters*', \
@@ -47,10 +42,11 @@ def main():
                  '*br.mobile.reuters*', '*mx.reuters*', '*live.reuters*', '*cn.reuters*', '*agency.reuters*', \
                  '*widerimage.reuters*']
 
+    crawls.reverse()
     for crawl in crawls:
         log(f"Starting pull for crawl: {crawl}")
 
-        cc_source = sources.CommonCrawl()
+        cc_source = sources.news.CommonCrawl(aws_access_key, aws_secret_key)
         cc_source.set_crawls(crawl)
         collector = collect.Collector(cc_source)
         collector.only_political_articles() \
@@ -59,7 +55,7 @@ def main():
                  .in_language(lang='eng') \
                  .distinct()
 
-        runner = run.Runner(collector, master_url=master_url, num_executors=5, executor_cores=8, executor_memory='64g', spark_conf=spark_conf)
+        runner = run.Runner(collector, master_url=master_url, num_executors=11, executor_cores=4, executor_memory='48g', driver_memory='64g', spark_conf=spark_conf)
         runner.send_to_database(db_connection_string, db_name, db_table)
         log(f"Finished pull for crawl: {crawl}")
 
--- scripts/generate_news2vec_graph.py
+++ scripts/generate_news2vec_graph.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+import time
 
 import networkx as nx
 from seldonite import sources, collect, nlp, graphs, run
@@ -21,7 +23,7 @@ def main():
     #}
 
     db_name = 'political_events'
-    db_table_in = 'reuters_news'
+    db_table_in = 'reuters_news_reduced'
 
     this_dir_path = os.path.dirname(os.path.abspath(__file__))
     data_path = os.path.join(this_dir_path, '..', 'data')
@@ -29,24 +31,38 @@ def main():
     if not os.path.exists(data_path):
         os.mkdir(data_path)
 
-    mongo_source = sources.MongoDB(db_connection_string, db_name, db_table_in, partition_size_mb=16)
-    #mongo_source = sources.CSV('/root/google_protest_news.csv')
-    collector = collect.Collector(mongo_source)
-    nl_processor = nlp.NLP(collector)
-    nl_processor.top_tfidf(20, save_path=os.path.join(data_path, 'idf_saved_models'))
+    first_year = 2019
+    last_year = 2020
+    for year in range(first_year, last_year):
+        print(f"Starting year {year}")
+        mongo_source = sources.news.MongoDB(db_connection_string, db_name, db_table_in, partition_size_mb=16)
+        #mongo_source = sources.CSV('/root/google_protest_news.csv')
+        
+        start_date = datetime.date(year, 1, 1)
+        end_date = datetime.date(year + 1, 1, 1)
+        
+        collector = collect.Collector(mongo_source) \
+            .in_date_range(start_date, end_date)
 
-    graph_constructor = graphs.Graph(nl_processor)
-    graph_constructor.build_news2vec_graph()
+        nl_processor = nlp.NLP(collector)
+        nl_processor.top_tfidf(20)
 
-    #runner = run.Runner(graph_constructor, master_url=master_url, num_executors=5, executor_cores=8, executor_memory='64g', driver_memory='64g', spark_conf=spark_conf)
-    runner = run.Runner(graph_constructor, driver_cores=24, driver_memory='64g', python_executable='/home/ndg/users/bsteel2/miniconda3/envs/seldonite/bin/python')
-    G, map_df = runner.get_obj()
-    
-    graph_path = os.path.join(data_path, 'all_articles.edgelist')
-    map_path = os.path.join(data_path, 'all_article_nodes.map')
+        graph_constructor = graphs.Graph(nl_processor)
+        graph_constructor.build_news2vec_graph()
 
-    nx.write_weighted_edgelist(G, graph_path)
-    map_df.to_csv(map_path, index=False, sep=' ', header=False)
+        runner = run.Runner(graph_constructor, master_url=master_url, num_executors=11, executor_cores=4, executor_memory='48g', driver_memory='64g', spark_conf=spark_conf)
+        #runner = run.Runner(graph_constructor, driver_cores=24, driver_memory='64g', python_executable='/home/ndg/users/bsteel2/miniconda3/envs/seldonite/bin/python')
+        G, map_df = runner.get_obj()
+        
+        graph_path = os.path.join(data_path, f"{str(year)[2:]}_all_articles.edgelist")
+        map_path = os.path.join(data_path, f"{str(year)[2:]}_all_article_nodes.map")
+
+        nx.write_weighted_edgelist(G, graph_path)
+        map_df.to_csv(map_path, index=False, sep=' ', header=False)
+        print(f"Finished year {year}")
+
+        # let spark get its act together
+        time.sleep(360)
 
 if __name__ == '__main__':
     main()
